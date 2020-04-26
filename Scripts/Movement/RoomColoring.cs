8463543572e6c49a6756c0f86d6389ebbfa72619
Alexander Makienko <thetarabbit@gmail.com>
2020-04-26 21:46:57 +0700

    Room coloring component

---
+++ Scripts/Movement/RoomColoring.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Fungible.Movement
+{
+    [RequireComponent(typeof(Room))]
+    public class RoomColoring : MonoBehaviour
+    {
+        [SerializeField] private Color color;
+        
+        private Color cachedColor;
+
+        private void OnEnable()
+        {
+            cachedColor = Map.Instance.GetSpriteRenderer().color;
+            Map.Instance.GetSpriteRenderer().color = color;
+        }
+
+        private void OnDisable()
+        {
+            Map.Instance.GetSpriteRenderer().color = cachedColor;
+        }
+    }
+}
