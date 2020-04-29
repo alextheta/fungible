9ab8c273a31b54e1b04ad0fb1d8abe3e5ffe31b4
Alexander Makienko <thetarabbit@gmail.com>
2020-04-29 23:30:53 +0700

    Resource unload fix

--- Scripts/Movement/Room.cs
+++ Scripts/Movement/Room.cs
@@ -22,7 +22,7 @@ namespace Fungible.Movement
 
         public void OnLeave()
         {
-            Resources.UnloadAsset(backgroundSprite);
+            Resources.UnloadUnusedAssets();
             LeaveEvent?.Invoke();
         }
 
