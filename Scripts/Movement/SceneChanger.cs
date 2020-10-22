6b0f1f0d9d5fe0a482e7fe615d80f1b7bc815209
Alexander Makienko <thetarabbit@gmail.com>
2020-10-23 01:50:25 +0700

    Scene changer

---
+++ Scripts/Movement/SceneChanger.cs
@@ -0,0 +1,16 @@
+﻿using Fungible.Events;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Fungible.Movement
+{
+    public class SceneChanger : BaseEventListener
+    {
+        [SerializeField] private string sceneMoveTo;
+
+        public override void Event()
+        {
+            SceneManager.LoadScene(sceneMoveTo);
+        }
+    }
+}
