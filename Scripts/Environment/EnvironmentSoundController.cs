015f8d7519cdbb106e6fcd42829e4093a9a8f1de
Alexander Makienko <thetarabbit@gmail.com>
2020-12-10 02:06:50 +0700

    Sounds WIP

---
+++ Scripts/Environment/EnvironmentSoundController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class EnvironmentSoundController : MonoBehaviour
+{
+    private AudioSource _audioSource;
+    public static EnvironmentSoundController Instance;
+    
+    private void Awake()
+    {
+        Instance = this;
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayClip(AudioClip clip)
+    {
+        _audioSource.clip = clip;
+        _audioSource.Play();
+    }
+}
---
+++ Scripts/Environment/SoundEventListener.cs
@@ -0,0 +1,16 @@
+﻿using Fungible;
+using Fungible.Events;
+using UnityEngine;
+
+public class SoundEventListener : BaseEventListener
+{
+    [SerializeField] private AudioClip _clip;
+
+    public override void Event()
+    {
+        if (!SaveController.LoadState)
+        {
+            EnvironmentSoundController.Instance.PlayClip(_clip);
+        }
+    }
+}
